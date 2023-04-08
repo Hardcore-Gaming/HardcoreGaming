c7dad050b863973eca44a7c1654c5e8b1557344c
ChrusKosco <ChrusKosco@users.noreply.github.com>
2023-04-07 17:13:47 -0800

    setting up registration
    add login and profile pages

---
+++ profiles/action_page.php
@@ -0,0 +1,3 @@
+Welcome <?php echo $_POST["user"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Your Password is: <?php echo $_POST["psw"]; ?>
---
+++ profiles/action_page_get.php
@@ -0,0 +1,3 @@
+Welcome Back <?php echo $_GET["user"]; ?><br>
+Email Correct: <?php echo $_GET["email"]; ?>
+Password Correct: <?php echo $_GET["psw"]; ?>
