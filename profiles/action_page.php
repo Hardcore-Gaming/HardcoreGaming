84fa63dd851b890b3fd0f5129768ac1c4d84f1f0
ChrusKosco <ChrusKosco@users.noreply.github.com>
2023-04-07 21:03:08 -0800

    add profiles
    add profile pages and user pages

--- profiles/action_page.php
+++ profiles/action_page.php
@@ -1,3 +1,3 @@
-Welcome <?php echo $_POST["user"]; ?><br>
+Welcome <?php echo $_POST["user"]; ?>
 Your email address is: <?php echo $_POST["email"]; ?>
 Your Password is: <?php echo $_POST["psw"]; ?>
