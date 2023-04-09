dd81294b68357bbc4d3c84be1285a7c15ac7113e
ChrusKosco <ChrusKosco@users.noreply.github.com>
2023-04-09 14:04:51 -0800

    login and register
    add login/register to forum and profile

--- profiles/action_page.php
+++ profiles/action_page.php
@@ -1,3 +1,8 @@
-Welcome <?php echo $_POST["user"]; ?>
+<?php
+$txt = "PHP";
+echo "I love $txt!";
+?>
+
+<!--Welcome <?php echo $_POST["user"]; ?>
 Your email address is: <?php echo $_POST["email"]; ?>
 Your Password is: <?php echo $_POST["psw"]; ?>
