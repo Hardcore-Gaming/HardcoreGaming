18107165d55d68b07feb19aee514071d46c4d328
ChrusKosco <60051006+ChrusKosco@users.noreply.github.com>
2023-11-10 10:36:25 -0900

    delete unused scripts
    action page
    moved examples

--- profiles/action_page.php
+++
@@ -1,8 +0,0 @@
-<?php
-$txt = "PHP";
-echo "I love $txt!";
-?>
-
-<!--Welcome <?php echo $_POST["user"]; ?>
-Your email address is: <?php echo $_POST["email"]; ?>
-Your Password is: <?php echo $_POST["psw"]; ?>
--- profiles/action_page_get.php
+++
@@ -1,3 +0,0 @@
-Welcome Back <?php echo $_GET["user"]; ?><br>
-Email Correct: <?php echo $_GET["email"]; ?>
-Password Correct: <?php echo $_GET["psw"]; ?>
